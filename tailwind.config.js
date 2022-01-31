51a726d0769ac8d471432071a2cb8740988ebb35
saetan <saetan@live.com>
2022-02-01 00:37:46 +0800

    first uwu

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
