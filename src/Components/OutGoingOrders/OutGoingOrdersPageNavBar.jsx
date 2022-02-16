d11d3e753d8538b2248e5ae4ddd259119724b341
saetan <saetan@live.com>
2022-02-16 15:48:49 +0800

    created outgoing order form

--- src/Components/OutGoingOrders/OutGoingOrdersPageNavBar.jsx
+++ src/Components/OutGoingOrders/OutGoingOrdersPageNavBar.jsx
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom";
+export default function OutGoingOrdersPageNavBar({
+  setToggleOutGoingOrdersForm,
+}) {
+  const toggleOutGoingOrdersPage = (event) => {
+    setToggleOutGoingOrdersForm(true);
+  };
+  const toggleOrderListPage = (event) => {
+    setToggleOutGoingOrdersForm(false);
+  };
 
-export default function OutGoingOrdersPageNavBar(props) {
   return (
     <nav className="flex flex-col  bg-atoll w-full items-center h-screen">
       <div>
         <span className="font-semibold text-2xl text-center text-azure">
           Outgoing Orders Page
         </span>
+      </div>
+      <div className="grid grid-cols-1 text-xl mt-5">
+        <button
+          onClick={toggleOutGoingOrdersPage}
+          className="block mt-4 lg:inline-block lg:mt-0 text-blackpearl hover:text-lightseagreen"
+        >
+          Create Outgoing Order
+        </button>
+        <button
+          onClick={toggleOrderListPage}
+          className="block mt-4 lg:inline-block lg:mt-0 text-blackpearl hover:text-lightseagreen"
+        >
+          Outgoing Orders List
+        </button>
       </div>
     </nav>
   );
--- src/Routes/OutgoingOrders.page.jsx
+++ src/Routes/OutgoingOrders.page.jsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
 import OutGoingOrdersPageNavBar from "../Components/OutGoingOrders/OutGoingOrdersPageNavBar";
+import OutGoingOrdersList from "../Components/OutGoingOrders/OutGoingOrdersList";
+import OutGoingOrdersForm from "../Components/OutGoingOrders/OutGoingOrdersForm";
 
 export default function OutGoingOrdersPage() {
   const [triggerRefresh, setTriggerRefresh] = useState(false);
+  const [isOutGoingOrdersForm, setToggleOutGoingOrdersForm] = useState(false);
 
   return (
     <div className="flex bg-oyesterbay">
       <div className="w-64">
-        <OutGoingOrdersPageNavBar />
+        <OutGoingOrdersPageNavBar
+          setToggleOutGoingOrdersForm={setToggleOutGoingOrdersForm}
+        />
+      </div>
+      <div className="w-full">
+        {isOutGoingOrdersForm ? (
+          <OutGoingOrdersForm
+            setTriggerRefresh={setTriggerRefresh}
+            triggerRefresh={triggerRefresh}
+          />
+        ) : (
+          <OutGoingOrdersList
+            triggerRefresh={triggerRefresh}
+            setTriggerRefresh={setTriggerRefresh}
+          />
+        )}
       </div>
     </div>
   );
